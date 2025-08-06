87fa2dbe95f36e646504005df6e5cd9c81d4aaf2
FernandoVini <>
2025-08-06 12:54:16 -0300

    criacao do segundo componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Ola from './Ola'
+import Tudo from './Tudo';
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Ola/>
+      <Tudo/>
     </>
   )
 }
